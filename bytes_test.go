ff826a37aa15cdf16e29024a782c790f19aab6c2
Matthias Loibl <mail@matthiasloibl.com>
2021-02-08 16:42:50 +0100

    Implement encoding.TextUnmarshaler for kong support

--- bytes_test.go
+++ bytes_test.go
@@ -41,6 +41,36 @@ func TestParseBase2Bytes(t *testing.T) {
 	assert.Equal(t, 1572864, int(n))
 }
 
+func TestBase2BytesUnmarshalText(t *testing.T) {
+	var n Base2Bytes
+	err := n.UnmarshalText([]byte("0B"))
+	assert.NoError(t, err)
+	assert.Equal(t, 0, int(n))
+	err = n.UnmarshalText([]byte("1kB"))
+	assert.Error(t, err)
+	err = n.UnmarshalText([]byte("1KB"))
+	assert.NoError(t, err)
+	assert.Equal(t, 1024, int(n))
+	err = n.UnmarshalText([]byte("1MB1KB25B"))
+	assert.NoError(t, err)
+	assert.Equal(t, 1049625, int(n))
+	err = n.UnmarshalText([]byte("1.5MB"))
+	assert.NoError(t, err)
+	assert.Equal(t, 1572864, int(n))
+
+	err = n.UnmarshalText([]byte("1kiB"))
+	assert.Error(t, err)
+	err = n.UnmarshalText([]byte("1KiB"))
+	assert.NoError(t, err)
+	assert.Equal(t, 1024, int(n))
+	err = n.UnmarshalText([]byte("1MiB1KiB25B"))
+	assert.NoError(t, err)
+	assert.Equal(t, 1049625, int(n))
+	err = n.UnmarshalText([]byte("1.5MiB"))
+	assert.NoError(t, err)
+	assert.Equal(t, 1572864, int(n))
+}
+
 func TestMetricBytesString(t *testing.T) {
 	assert.Equal(t, MetricBytes(0).String(), "0B")
 	// TODO: SI standard prefix is lowercase "kB"
